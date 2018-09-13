a392e15c4e98a0e1626bb2cc72bb5f56ecb3ffe6
Utkarsh Dhawan <udhawan@tavisca.com>
2018-09-13 21:45:14 +0530

    TicTacToe Game Implementation using Web Api

---
+++ TicTacToe/TicTacToe/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string userName { get; set; }
+        public string Email { get; set; }
+        public string accessToken { get; set; }
+        public bool status { get; set; }
+    }
+}
